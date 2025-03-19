1f1742d59e61c808ed165b4f9177744366526a41
goura <gourabdasgdas2010@gmail.com>
2025-03-19 19:44:16 +0100

    First Commit

---
+++ src/main/java/org/gd/devops/prac/Calc.java
@@ -0,0 +1,20 @@
+package org.gd.devops.prac;
+
+public class Calc {
+	
+	public int sum(int a, int b) {
+		return a+b;
+	}
+	
+	public int subtract(int a, int b) {
+		return a-b;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Calc myCalc=new Calc();
+		System.out.println("Sum = "+myCalc.sum(15, 5));
+		System.out.println("Difference = "+myCalc.subtract(15, 5));
+	}
+
+}
