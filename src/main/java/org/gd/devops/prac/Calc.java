7f5ccaa274a1d31a51c8e2a02bbf84b46f34b202
goura <gourabdasgdas2010@gmail.com>
2025-03-21 08:50:45 +0100

    Modulus function added

--- src/main/java/org/gd/devops/prac/Calc.java
+++ src/main/java/org/gd/devops/prac/Calc.java
@@ -14,13 +14,18 @@ public class Calc {
 		return a/b;
 	}
 	
+	public int modulus(int a, int b) {
+		return a%b;
+	}
+
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Calc myCalc=new Calc();
 		System.out.println("Sum = "+myCalc.sum(15, 5));
 		System.out.println("Difference = "+myCalc.subtract(15, 5));
 		System.out.println("Division = "+myCalc.division(15, 5));
-		
+		System.out.println("Modulus = "+myCalc.modulus(15, 5));
 	}
 
 }
